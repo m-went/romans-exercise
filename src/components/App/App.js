42103cdbcec83e12d800abc8df2fd0651c4bb798
Michał Went <wentmichal@gmail.com>
2022-10-23 22:20:30 +0200

    delete some CRA code and add UserListItem component

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserListItem from '../UserListItem/UserListItem';
+
+function App() {
+  return (
+    <div className="App">
+      <UserListItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserListItem/UserListItem.js
@@ -0,0 +1,5 @@
+function UserListItem(props) {
+  return <h1>There will be list of users! </h1>;
+}
+
+export default UserListItem;
