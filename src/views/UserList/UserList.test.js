e241c77712627bc725e65ac34b828feef99fb96e
Michał Went <wentmichal@gmail.com>
2022-10-31 12:18:45 +0100

    change UserList test file. Also try to test delete function, but with context and actual file structure it is impossible

--- src/views/UserList/UserList.test.js
+++ src/views/UserList/UserList.test.js
@@ -1,4 +1,4 @@
-import { screen, render, fireEvent } from '@testing-library/react';
+import { screen, render, cleanup } from '@testing-library/react';
 import UserList from './UserList';
 
 const mockUsersProp = [
@@ -11,9 +11,18 @@ const mockUsersProp = [
 ];
 
 describe('userList tests', () => {
+  afterEach(cleanup);
   test('render component', () => {
     render(<UserList users={mockUsersProp} />);
     const headerElement = screen.getByRole('heading', { name: 'Students List' });
     expect(headerElement).toBeInTheDocument();
+  });
+
+  test('check if mock users are properly rendered', () => {
+    render(<UserList users={mockUsersProp} />);
+    const firstNameElement = screen.getByText('Michał Went');
+    const secondNameElement = screen.getByText('Adam Romański');
+    expect(firstNameElement).toBeInTheDocument();
+    expect(secondNameElement).toBeInTheDocument();
   });
 });
